95e2f068cf95dab7b5438b17f498e92f6cfd28fd
nyarikikaruma <nyarikikaruma@gmail.com>
2023-11-02 12:32:00 +0300

    Documentation added

---
+++ docGuide.js
@@ -0,0 +1,114 @@
+// Javascript for beginners
+// Chapter 1: Introduction to JavaScript
+// Video 1: Course Overview - Why learn JavaScript?
+// Video 2: Course Overview - Expected Course Outcomes
+// Video 3: History of JavaScript - From Java to JavaScript
+// Video 4: History of JavaScript - Evolution of ECMAScript
+// Video 5: Setting Up Your Environment - Installing the Browser & Editor
+// Video 6: Setting Up Your Environment - Writing & Executing Your First Script
+// Chapter 2: Basics of JavaScript
+// Video 7: Syntax and Statements - Basic Syntax Rules
+// Video 8: Syntax and Statements - Statements vs. Expressions
+// Video 9: Variables and Data Types - Declaring Variables
+// Video 10: Variables and Data Types - Different Data Types
+// Video 11: Variables and Data Types - Type Coercion and Conversion
+// Video 12: Operators - Arithmetic Operators
+// Video 13: Operators - Comparison Operators
+// Video 14: Operators - Logical Operators
+// Chapter 3: Control Flow
+// Video 15: Conditional Statements - if Statement Basics
+// Video 16: Conditional Statements - Advanced if, else, and else if
+// Video 17: Conditional Statements - The switch Statement
+// Video 18: Loops - Introducing the for Loop
+// Video 19: Loops - The while and do-while Difference
+// Chapter 4: Functions
+// Video 20: Understanding Functions - What Are Functions?
+// Video 21: Understanding Functions - Declaring and Invoking Functions
+// Video 22: Dive into Function Details - Parameters and Arguments
+// Video 23: Dive into Function Details - Return Statements
+// Video 24: Dive into Function Details - Function Scope and Closures
+// Video 25: Modern JavaScript Functions - Introduction to Arrow Functions
+// Video 26: Modern JavaScript Functions - Using Callbacks and Higher-Order Functions
+
+// Chapter 5: Objects and Arrays
+// Video 27: Working with Objects - Object Basics: Properties and Methods
+// Video 28: Working with Objects - Navigating Object Literals
+// Video 29: Working with Objects - Understanding this in Objects
+// Video 30: Mastering Arrays - Array Basics and Manipulation
+// Video 31: Mastering Arrays - Iterating Over Arrays
+// Video 32: Mastering Arrays - Array Methods Deep Dive
+// Chapter 6: DOM Manipulation
+// Video 33: Understanding the DOM - What is the DOM?
+// Video 34: Understanding the DOM - Exploring Document Object
+// Video 35: Manipulating Web Pages - Selecting Elements
+// Video 36: Manipulating Web Pages - Modifying Element Content
+// Video 37: Manipulating Web Pages - Creating and Removing Elements
+// Chapter 7: Events
+// Video 38: Grasping JavaScript Events - Understanding Event Handling
+// Video 39: Grasping JavaScript Events - Working with Event Listeners
+// Video 40: Grasping JavaScript Events - Event Propagation: Bubbling and Capturing
+// Chapter 8: Forms and Validation
+// Video 41: Handling HTML Forms - Form Structure and Elements
+// Video 42: Handling HTML Forms - Submitting Forms
+// Video 43: Handling HTML Forms - Preventing Default Form Behavior
+// Video 44: Validating Form Data - Basic Input Validation
+// Video 45: Validating Form Data - Using Regular Expressions for Validation
+// Video 46: Validating Form Data - Custom Validation Feedback
+// Chapter 9: Error Handling and Debugging
+// Video 47: Tackling Errors in JavaScript - Common JavaScript Errors
+// Video 48: Tackling Errors in JavaScript - Using try...catch Statements
+// Video 49: Tackling Errors in JavaScript - Throwing Custom Errors
+// Video 50: Debugging JavaScript - Debugging Basics
+// Video 51: Debugging JavaScript - Using Browser Developer Tools
+// Video 52: Debugging JavaScript - Advanced Debugging Techniques
+
+// Chapter 10: Asynchronous JavaScript
+// Video 53: Understanding Asynchrony - Synchronous vs. Asynchronous Programming
+// Video 54: Understanding Asynchrony - Call Stack and Event Loop
+// Video 55: Working with Promises - Creating and Using Promises
+// Video 56: Working with Promises - Chaining Promises
+// Video 57: Working with Promises - Handling Errors in Promises
+// Video 58: Modern Async Patterns - Introduction to Async/Await
+// Video 59: Modern Async Patterns - Error Handling with Async/Await
+// Chapter 11: APIs and Fetching Data
+// Video 60: What Are APIs? - API Basics
+// Video 61: What Are APIs? - RESTful Services Overview
+// Video 62: Communicating with APIs - Using the Fetch API
+// Video 63: Communicating with APIs - Handling API Responses (JSON)
+// Video 64: Communicating with APIs - Error Handling with Fetch
+// Chapter 12: Best Practices
+// Video 65: Writing Clean and Efficient Code - Code Organization Techniques
+// Video 66: Writing Clean and Efficient Code - Commenting and Documentation
+// Video 67: Performance and Security - Optimizing Performance
+// Video 68: Performance and Security - Basic Security Practices in JavaScript
+// Chapter 13: Final Project - ShopMate Application
+// Video 69: ShopMate Project Introduction - Understanding the Project Scope
+// Explaining the need for an application like ShopMate.
+// Overview of ShopMate: create, read, update, and delete lists and items
+// Video 70: Environment Setup for ShopMate - Preparing for Development
+// Setting up SQLite (or a similar database) for local data management
+// Video 71: Building ShopMate - Phase 1: Designing the Application Structure
+// Planning the application structure.
+// Designing the UI/UX for easy navigation and usage.
+// Establishing database schemas for shopping lists and checklists
+// Video 72: Building ShopMate - Phase 2: Implementing Core Functionalities
+// Coding the CRUD operations for shopping lists
+// Implementing the ability to create, read, update, and delete checklists for lists
+// Ensuring data consistency and integrity during operations.
+// Video 73: Building ShopMate - Phase 3: Advanced Features and Testing
+// Developing the checkout feature for items on the shopping lists
+// Implementing data storage with SQLite
+// Testing the application for bugs and user experience inconsistencies
+// Video 74: ShopMate Project Review - Effective Code Review Strategies
+// Code review to ensure code quality, maintainability, and readability.
+// Identifying common pitfalls in code and how to avoid them
+// Video 75: Refactoring ShopMate - Improving Code and Performance
+// Analyzing current codebase for areas of improvement.
+// Refactoring code to enhance performance, readability, and scalability
+// Final testing after refactoring and preparing the application for deployment
+// Chapter 14: Conclusion and Next Steps
+// Video 76: Course Recap - Reviewing Key Takeaways
+// Video 77: Course Recap - How to Keep Improving
+// Video 78: Further Learning and Resources - Recommended Books and Courses
+// Video 79: Further Learning and Resources - Online Communities and Events
+
