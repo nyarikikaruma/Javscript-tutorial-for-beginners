de77e4f5df37674719323398030a65b692c4386b
nyarikikaruma <nyarikikaruma@gmail.com>
2023-11-14 09:12:49 +0300

    THe dom added

---
+++ test.js
@@ -0,0 +1,6 @@
+const myArr = "abcdefghijkl";
+const newArr = myArr.substring(0, 2);
+
+console.log(myArr);
+
+console.log(newArr);
---
+++ theDom/script.js
@@ -0,0 +1,8 @@
+let listNodeItem = document.getElementById('listOne');
+
+let newItemNode  = document.getElementById('listOne')
+
+console.log(newItemNode);
+listNodeItem.children[0].innerHTML = 'Kamau Mwakania'
+
+console.log(listNodeItem.children[0].innerHTML);
