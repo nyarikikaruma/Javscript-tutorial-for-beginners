47e6f0618f30cb9d3b4f9bedaee91e8a2baee5f3
nyarikikaruma <nyarikikaruma@gmail.com>
2023-11-24 10:51:01 +0300

    Error handling added

--- forms/preventDefault.js
+++ forms/preventDefault.js
@@ -1,4 +1,11 @@
-
+let x = 5;
+try {
+  x = y + 1;   // y cannot be used (referenced)
+}
+catch(err) {
+  console.log(err);
+  document.getElementById("demo").innerHTML = err.name;
+}
 // JavaScript
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting
